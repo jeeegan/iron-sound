6eab2f7d738122761bd24c6f4a071e39c1dabc66
Alec Budd <abudd1094@gmail.com>
2019-02-27 11:23:16 +0100

    updated upload model

---
+++ server/models/Upload.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const uploadSchema = new Schema({
+  title: { 
+    type: String, 
+    required: true 
+  },
+  artist: { 
+    type: String, 
+    required: true 
+  },
+  album: String,
+  year: { 
+    type: String, 
+    required: true 
+  },
+  genre: { 
+    type: String, 
+    required: true 
+  },
+  tags: [String],
+  embed_url: { 
+    type: String, 
+    required: true, 
+    match: /^https?\:\/\// // Must start by "http://" or "https://" }
+  },
+  host: {
+    type: String,
+    required: true,
+    enum: ['bc', 'sc', 'yt'],
+    _created_by: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  upload_img: String,
+  upload_type: {
+    type: String,
+    required: true,
+    enum: ['track', 'album']
+  }
+}, {
+    timestamps: {
+      createdAt: 'created_at',
+      updatedAt: 'updated_at'
+    }
+  });
+
+const Upload = mongoose.model('Upload', uploadSchema);
+module.exports = Upload;
