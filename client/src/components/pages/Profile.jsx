2f28bc9452b9c1a76c06da59f871c5a784fa56df
Alec Budd <abudd1094@gmail.com>
2019-02-28 12:49:46 +0100

    styling basics

--- client/src/components/pages/Profile.jsx
+++ client/src/components/pages/Profile.jsx
@@ -15,15 +15,25 @@ class Profile extends Component {
   
   render() {     
     return (
-      <div className="Profile">
-        <h2>Profile</h2>
-        {this.state.display_name} <br/>
-        {this.state.user_img} <br/>
+      <div className="pageContent">
+        <div className="profile-header">
+          <div className="user-img">
+            {this.state.user_img}
+          </div>
+          <h2>{this.state.display_name}</h2>
+        </div>
+        
+        
+        
         {this.state.location} <br/>
         {this.state.bio} <br/>
-        {this.state.bc_url} <br/>
-        {this.state.sc_url} <br/>
-        {this.state.yt_url} <br/>
+        
+        <a href={this.state.sc_url} target="_blank" rel="noopener noreferrer"><img style={{width: "3vw"}} src="/icon-sc.png" alt="soundcloud link"/></a> <br/>
+
+        <a href={this.state.bc_url} target="_blank" rel="noopener noreferrer"><img style={{width: "2.5vw"}}src="/icon-bc.png" alt="bandcamp link"/></a>
+        <br/>
+       
+        <a href={this.state.yt_url} target="_blank" rel="noopener noreferrer"><img style={{width: "3vw"}} src="/icon-yt.png" alt="youtube link"/></a> <br/>
       </div>
     );
   }
