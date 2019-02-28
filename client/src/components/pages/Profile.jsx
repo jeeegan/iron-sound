63bdd654b0221646bacb90dd387fe2e2d4164164
Alec Budd <abudd1094@gmail.com>
2019-02-28 18:11:57 +0100

    user image displays on profile

--- client/src/components/pages/Profile.jsx
+++ client/src/components/pages/Profile.jsx
@@ -39,7 +39,7 @@ class Profile extends Component {
           </div>
           {/* USER IMAGE */}
           <div className="user-img">
-            <img style={{width: '24vw', height: 'auto', borderRadius: '50%'}}src="/avatar-test.jpg" alt=""/>
+            <img style={{width: '24vw', height: 'auto', borderRadius: '50%'}}src={this.state.user_img} alt=""/>
           </div>
         </div>
       </div>
