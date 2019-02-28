344053a0de1fe1932b7785922be2a07cf44d3c22
Alec Budd <abudd1094@gmail.com>
2019-02-28 11:58:45 +0100

    basic navbar reformat

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,13 +4,17 @@ import { Link, NavLink } from 'react-router-dom';
 
 const Navbar = () => {
     return(
-      <div>
-        <NavLink to="/" exact>Home</NavLink>
+      <div className="navbar">
+        <div className="nav-left">
+          <NavLink to="/" exact>Home</NavLink>
           {api.isLoggedIn() && <NavLink to="/upload">Upload</NavLink>}
           {api.isLoggedIn() && <NavLink to="/profile">Profile</NavLink>}
+        </div>
+        <div className="nav-right">
           {!api.isLoggedIn() && <NavLink to="/signup">Signup</NavLink>}
           {!api.isLoggedIn() && <NavLink to="/login">Login</NavLink>}
           {api.isLoggedIn() && <Link to="/" onClick={(e) => this.handleLogoutClick(e)}>Logout</Link>}
+        </div>
       </div>
     )
 }
