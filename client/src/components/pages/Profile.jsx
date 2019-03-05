b03b3ac61a11163647971ed4f130a76b4968d869
Alec Budd <abudd1094@gmail.com>
2019-03-05 15:33:26 +0100

    styling

--- client/src/components/pages/Profile.jsx
+++ client/src/components/pages/Profile.jsx
@@ -51,9 +51,13 @@ class Profile extends Component {
           </div>
         </div>
         <div>
-          <h5>Uploads:</h5>
           {this.state.uploads && this.state.uploads.map( upload => {
-            return <Wavesurfer key={upload._id} identifier={upload._id} upload_url={upload.upload_url} artist={upload.artist} title={upload.title}/>
+            return <Wavesurfer 
+                key={upload._id} 
+                identifier={upload._id} 
+                upload_url={upload.upload_url} artist={upload.artist} 
+                title={upload.title}
+              />
           })}
         </div>
 
